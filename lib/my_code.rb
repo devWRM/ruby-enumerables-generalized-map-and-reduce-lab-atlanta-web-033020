2a251c0fd35ecf7515df9e5d96ff657e8636cc0b
Wendy Raven McNair <wen.mcnair@gmail.com>
2020-03-14 21:44:04 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -9,9 +9,30 @@ def map(array)
   
   while i < array.length do
     
-    # yield passes each element to the block
+      # yield passes each element to the block
    new.push(yield(array[i]))
-    # push the element into the new array
+      # push the element into the new array
+    
+    i += 1
+    
+    # Each method call with a block will do the actual calculations
+   
+  end
+  new
+end
+
+
+
+
+def reduce(array)
+  total = 0
+  i = 0
+  
+  while i < array.length do
+    
+      # yield passes each element to the block
+   new.push(yield(array[i]))
+      # push the element into the new array
     
     i += 1
     
@@ -20,5 +41,10 @@ def map(array)
   end
   new
 end
+
+
+
+
+
 
 
