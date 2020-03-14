97737a0e6c66d3697365936485a28a34c7c83103
Wendy Raven McNair <wen.mcnair@gmail.com>
2020-03-14 21:52:42 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -25,21 +25,9 @@ end
 
 
 def reduce(array, start_val = 0)
-  total = 0
-  i = 0
   
-  while i < array.length do
-    
-      # yield passes each element to the block
-   total += (yield(array[i]))
-      # push the element into the new array
-    
-    i += 1
-    
-    # Each method call with a block will do the actual calculations
-   
-  end
-  total
+  
+  
 end
 
 
